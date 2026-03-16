d87ccd057fb45647a16fc822bfa48082196642d3
Divine Davis <divinejdavis@gmail.com>
2026-03-16 13:55:09 -0400

    Initial Commit

---
+++ Hidden Gems/Hidden_GemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hidden_GemsApp.swift
+//  Hidden Gems
+//
+//  Created by Divine Davis on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Hidden_GemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
